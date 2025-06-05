78e9fa09dfe55d30f1a677e6926c65c8277235b1
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-06-05 20:09:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl">Hello World!</h1>
+      <Button variant="default" className="p-10 ml-3">
+        Click me
+      </Button>
+    </>
+  );
+}
+
+export default App;
